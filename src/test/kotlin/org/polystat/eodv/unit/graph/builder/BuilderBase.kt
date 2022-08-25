5792450117236bcef15603870b3da664ce3d8f6d
OlesiaSub <lesya.sub@mail.ru>
2022-08-25 11:20:54 +0300

    DiKTat checks done

--- src/test/kotlin/org/polystat/eodv/unit/graph/builder/BuilderBase.kt
+++ src/test/kotlin/org/polystat/eodv/unit/graph/builder/BuilderBase.kt
@@ -39,7 +39,7 @@ import java.nio.file.Paths
  * Base class for graph builder testing
  */
 open class BuilderBase : TestBase {
-    private val logger = KotlinLogging.logger {}
+    private val logger = KotlinLogging.logger(this.javaClass.name)
 
     override fun doTest() {
         val path = getTestName()

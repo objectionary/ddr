b57d33f8a7523ed23bccf800b8ce691f36fcb46f
OlesiaSub <lesya.sub@mail.ru>
2022-09-02 12:30:47 +0300

    fixes

--- src/main/kotlin/org/objectionary/ddr/graph/CondAttributesSetter.kt
+++ src/main/kotlin/org/objectionary/ddr/graph/CondAttributesSetter.kt
@@ -1,16 +1,19 @@
 package org.objectionary.ddr.graph
 
 import org.w3c.dom.Document
+import org.w3c.dom.Node
 
 class CondAttributesSetter(
     private val graph: Graph,
     private val documents: MutableMap<Document, String>
 ) {
-    private fun collectDeclarations() {
+    private val conditions : MutableSet<Node> = mutableSetOf()
+
+    private fun collectConditions() {
         val objects = graph.initialObjects
         for (node in objects) {
             val base = base(node) ?: continue
-            if (abstract(node) == null && !base.startsWith('.')) {
+            if (base == ".if") {
 //                declarations[node] = null
             }
         }

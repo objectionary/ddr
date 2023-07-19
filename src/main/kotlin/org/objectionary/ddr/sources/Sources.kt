5f8ebd37dcaa660628befc0f2e8b917f9a871050
c71n93 <korostinskii.r@phystech.edu>
2023-07-19 13:30:32 +0300

    edit todos

--- src/main/kotlin/org/objectionary/ddr/sources/Sources.kt
+++ src/main/kotlin/org/objectionary/ddr/sources/Sources.kt
@@ -48,7 +48,7 @@ class SrsTransformed(
      *
      * @return [MutableMap] with collected [documents] as key and their filenames as value
      *
-     * @todo #121:30min `createTempDirectories` method is redundantly called in a loop. This method should be called once.
+     * @todo #119:30min `createTempDirectories` method is redundantly called in a loop. This method should be called once.
      */
     override fun walk(): MutableMap<Document, String> {
         Files.walk(inPath)
@@ -83,8 +83,8 @@ class SrsTransformed(
      * @param filename path to current xmir file in source directory
      * @return path to the temporary directory with xmir files
      *
-     * @todo #121:30min if output directory already exists createFile throws exception and logger prints error message. This situation should be handled correctly.
-     * @todo #121:30min rename "tmpPath" to "resPath" and make it a public field.
+     * @todo #119:30min if output directory already exists createFile throws exception and logger prints error message. This situation should be handled correctly.
+     * @todo #119:30min rename "tmpPath" to "resPath" and make it a public field.
      */
     private fun createTempDirectories(filename: String): String {
         val tmpPath = generateTmpPath(filename)
